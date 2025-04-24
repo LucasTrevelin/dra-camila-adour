7192b5675c05717795fe624be2a28c42acc668a4
Lucas Henrique Trevelin <lucas-henrique_venturus>
2025-04-24 11:34:23 -0300

    refact: change the tag managers

---
+++ dra-camila-adour/src/components/Header/HeaderLinks.ts
@@ -0,0 +1,17 @@
+import { TLinkProps } from './Link/Link.types'
+
+export const headerLinks: TLinkProps[] = [
+  { title: 'Home' },
+  { title: 'Tratamentos oferecidos' },
+  { title: 'Equipamentos/Aparelhos' },
+  { title: 'Sobre a Dra. Juliana Madeira' },
+  { title: 'Contato' }
+]
+
+export const headerMenuLinks: TLinkProps[] = [
+  { title: 'Contato', variant: 'hamburguer' },
+  { title: 'Home', variant: 'hamburguer' },
+  { title: 'Tratamentos oferecidos', variant: 'hamburguer' },
+  { title: 'Equipamentos/Aparelhos', variant: 'hamburguer' },
+  { title: 'Sobre a Dra. Juliana Madeira', variant: 'hamburguer' }
+]
